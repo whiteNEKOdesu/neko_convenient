22f7873f2ec3d2eb038d5d245e1071a33f283b02
NEKO <2665249580@qq.com>
2023-03-31 16:50:59 +0800

    vesrion_1.1.1 product update

---
+++ neko-convenient-product8005/src/main/java/neko/convenient/nekoconvenientproduct8005/NekoConvenientProduct8005Application.java
@@ -0,0 +1,13 @@
+package neko.convenient.nekoconvenientproduct8005;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NekoConvenientProduct8005Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NekoConvenientProduct8005Application.class, args);
+    }
+
+}
---
+++ neko-convenient-product8005/src/test/java/neko/convenient/nekoconvenientproduct8005/NekoConvenientProduct8005ApplicationTests.java
@@ -0,0 +1,13 @@
+package neko.convenient.nekoconvenientproduct8005;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NekoConvenientProduct8005ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
